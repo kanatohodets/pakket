02de15b98cc4223b1b1304a8efa5053bf182653e
Dmitry Ulanov <dmitry.ulanov@booking.com>
2017-12-11 15:35:36 +0100

    Add releases in versioner

--- lib/Pakket/Versioning/Perl.pm
+++ lib/Pakket/Versioning/Perl.pm
@@ -8,7 +8,12 @@ use version 0.77;
 with qw< Pakket::Role::Versioning >;
 
 sub compare {
-    return version->parse( $_[1] ) <=> version->parse( $_[2] );
+    my ($ver1, $rel1) = split(/:/, $_[1]);
+    my ($ver2, $rel2) = split(/:/, $_[2]);
+    $rel1 //= 1;
+    $rel2 //= 1;
+    return (version->parse($ver1) <=> version->parse($ver2)
+            or $rel1 <=> $rel2);
 }
 
 no Moose;
