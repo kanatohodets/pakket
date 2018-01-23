af8d379cbc460154969b2c221df63902fc9c4a17
Sawyer X <xsawyerx@cpan.org>
2018-01-23 16:05:57 +0100

    Add documentation for Pakket::Repository::Backend::HTTP

--- lib/Pakket/Repository/Backend/HTTP.pm
+++ lib/Pakket/Repository/Backend/HTTP.pm
@@ -216,3 +216,195 @@ no Moose;
 __END__
 
 =pod
+
+=head1 SYNOPSIS
+
+    my $backend = Pakket::Repository::Backend::HTTP->new(
+        'host'        => 'your.pakket.subdomain.company.com',
+        'port'        => '80',
+        'base_path'   => '/pakket/,
+        'http_client' => HTTP::Tiny->new(...),
+    );
+
+    # Handling locations
+    $backend->store_location( $id, $path_to_file );
+    my $path_to_file = $backend->retrieve_location($id);
+    $backend->remove_location($id);
+
+    # Handling content
+    $backend->store_content( $id, $structure );
+    my $structure = $backend->retrieve_content($id);
+    $backend->remove_content($id);
+
+    # Getting data
+    my $ids = $backend->all_object_ids; # [ ... ]
+    my $ids = $backend->all_object_ids_by_name( 'Path::Tiny', 'perl' );
+    if ( $backend->has_object($id) ) {
+        ...
+    }
+
+=head1 DESCRIPTION
+
+This repository backend will use HTTP to store and retrieve files and
+content structures. It is useful when you are using multiple client
+machines that need to either build to a remote repository or install
+from a remote repository.
+
+On the server side you will need to use L<Pakket::Web>.
+
+=head1 ATTRIBUTES
+
+When creating a new class, you can provide the following attributes:
+
+=head2 host
+
+Hostname or IP string to use.    
+
+This is a required parameter.
+
+=head2 port
+
+The port on which the remote server is listening.
+
+This is a required parameter.
+
+=head2 base_path
+
+The default path to prepend to the request URL. This is useful when you
+serve it on a server that also serves other content, or when you have
+multiple pakket instances and they are in subdirectories.
+
+Default: empty.
+
+=head2 base_url
+
+This is an advanced attribute that is generated automatically from the
+C<host>, C<port>, and C<base_path>. This uses B<http> by default but
+you can create your own with B<https>:
+
+    my $backend = Pakket::Repository::Backend::HTTP->new(
+        'base_path' => 'https://my.path:80/secure_packages/',
+    );
+
+Default: B<<C<http://HOST:PORT/BASE_URL>>>.
+
+=head2 http_client
+
+This is an advanced attribute defining the user agent to be used for
+fetching or updating data. This uses L<HTTP::Tiny> so you need one that
+is compatible or a subclass of it.
+
+Default: L<HTTP::Tiny> object.
+
+=head1 METHODS
+
+All examples below use a particular string as the ID, but the ID could
+be anything you wish. Pakket uses the package ID for it, which consists
+of the category, name, version, and release.
+
+=head2 store_location
+
+    $backend->store_location(
+        'perl/Path::Tiny=0.100:1',
+        '/tmp/myfile.tar.gz',
+    );
+
+This method makes a request to the server in the path
+C</store/location?id=$ID>. The C<$ID> is URI-escaped and the request
+is made as a C<x-www-form-urlencoded> request.
+
+The request is guarded by a check that will report this error, making
+the return value is useless.
+
+=head2 retrieve_location 
+
+    my $path = $backend->retrieve_location('perl/Path::Tiny=0.100:1');
+
+This method makes a request to the server in the path
+C</retrieve/location?id=$ID>. The C<$ID> is URI-escaped.
+
+A temporary file is then created with the content and the method
+returns the location of this file.
+
+=head2 remove_location
+
+    $backend->remove_location('perl/Path::Tiny=0.100:1');
+
+This method makes a request to the server in the path
+C</remove/location?id=$ID>. The C<$ID> is URI-escaped.
+
+The return value is a boolean of the success or fail of this operation.
+
+=head2 store_content
+
+    $backend->store_content(
+        'perl/Path::Tiny=0.100:1',
+        {
+            'Package' => {
+                'category' => 'perl',
+                'name'     => 'Path::Tiny',
+                'version'  => 0.100,
+                'release'  => 1,
+            },
+
+            'Prereqs' => {...},
+        },
+    );
+
+This method makes a POST request to the server in the path
+C</store/content>. The request body contains the content, encoded
+into JSON. This means you cannot store objects, only plain structures.
+
+The request is guarded by a check that will report this error, making
+the return value is useless. To retrieve the content, use
+C<retrieve_content> described below.
+
+=head2 retrieve_content
+
+    my $struct = $backend->retrieve_content('perl/Path::Tiny=0.100:1');
+
+This method makes a request to the server in the path
+C</retrieve/content?id=$ID>. The C<$ID> is URI-escaped.
+
+It then returns the content as a structure, unserialized.
+
+=head2 remove_content
+
+    my $success = $backend->remove_content('perl/Path::Tiny=0.100:1');
+
+This method makes a request to the server in the path
+C</remove/content?id=$ID>. The C<$ID> is URI-escaped.
+
+The return value is a boolean of the success or fail of this operation.
+
+=head2 all_object_ids
+
+    my $ids = $backend->all_object_ids();
+
+This method makes a request to the server in the path
+C</all_object_ids> and returns all the IDs of objects it stores in an
+array reference. This helps find whether an object is available or not.
+
+=head2 all_object_ids_by_name
+
+    my $ids = $backend->all_object_ids_by_name( $name, $category );
+
+This is a more specialized method that receives a name and category for
+a package and locates all matching IDs.
+
+This method makes a request to the server in the path
+C</all_object_ids_by_name?name=$NAME&category=$CATEGORY>. The
+C<$NAME> and C<$CATEGORY> are URI-escaped.
+
+It then returns all the IDs it finds in an array reference.
+
+You do not normally need to use this method.
+
+=head2 has_object
+
+    my $exists = $backend->has_object('perl/Path::Tiny=0.100:1');
+
+This method receives an ID and returns a boolean if it's available.
+
+This method makes a request to the server in the path
+C</has_object?id=$ID>. The C<$ID> is URI-escaped.
